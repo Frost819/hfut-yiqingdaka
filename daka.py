7318800942dac1ad92b1927291f32060eb6a9176
Frost <894785732@qq.com>
2021-12-23 23:44:57 +0800

    加入检查打卡是否成功功能

--- daka.py
+++ daka.py
@@ -7,6 +7,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
+#等待按钮出现并点击
+def wait_and_click(Xpath):
+    wait = WebDriverWait(browser, 5, 0.5)
+    wait.until(lambda browser: browser.find_element(By.XPATH, Xpath))
+    browser.find_element(By.XPATH, Xpath).click()
+
 #命令参数
 parser = argparse.ArgumentParser(description='定时自动打卡')
 parser.add_argument('--username', help='学号')
@@ -33,14 +39,11 @@ while True:
     if now_time.hour == hour and now_time.minute == minute or args.once:
         print('开始打卡')
         url="http://stu.hfut.edu.cn/xsfw/sys/emapfunauth/pages/welcome.do?service=http%3A%2F%2Fstu.hfut.edu.cn%2Fxsfw%2Fsys%2Fxggzptapp%2F*default%2Findex.do%3Fmin%3D1#/"
-        browser=webdriver.Chrome(executable_path="./chromedriver", chrome_options=chrome_options)
+        browser=webdriver.Chrome(executable_path="./chromedriver")
         #进入首页
         browser.get(url)
-        wait = WebDriverWait(browser, 5, 0.5)
-        wait.until(lambda browser: browser.find_element(By.XPATH, '//*[@id="page"]/div/div/div/div/button[1]'))
-        button = browser.find_element(By.XPATH, '//*[@id="page"]/div/div/div/div/button[1]')
         #进入登录界面
-        button.click()
+        wait_and_click('//*[@id="page"]/div/div/div/div/button[1]')
         #输入框
         input_username = browser.find_element(By.XPATH, '//*[@id="username"]')
         input_username.send_keys(args.username)
@@ -50,18 +53,22 @@ while True:
         #登录成功
         if 'cas.hfut.edu.cn' not in browser.current_url:
             #转到疫情信息收集页面
-            browser.get('http://stu.hfut.edu.cn/xsfw/sys/swmxsyqxxsjapp/*default/index.do#')
-            wait = WebDriverWait(browser, 5, 0.5)
+            browser.get('http://stu.hfut.edu.cn/xsfw/sys/swmxsyqxxsjapp/*default/index.do#/add')
             try:
-                wait.until(lambda browser: browser.find_element(By.XPATH, '//*[@id="app"]/div[1]/div/div[6]/div/button'))
+                #点击提交按钮
+                wait_and_click('//*[@id="app"]/div[1]/div/div[6]/div/button')
             except Exception:
-                print('Error:打卡失败，请确认是否已打过卡')
+                print('Error:打卡失败!')
                 browser.quit()
             else:
-                submit_button = browser.find_element(By.XPATH, '//*[@id="app"]/div[1]/div/div[6]/div/button')
-                #点击提交按钮
-                submit_button.click()
-                print('打卡成功!')
+                #点击返回转到index页面
+                wait_and_click('//*[@id="app"]/div[1]/div/div[2]/button')
+                wait = WebDriverWait(browser, 5, 0.5)
+                wait.until(lambda browser: browser.find_element(By.XPATH, '//*[@id="scroll_container"]/div[1]/div[1]/div[1]/div'))
+                #获取最新一次打卡日期
+                text = browser.find_element(By.XPATH, '//*[@id="scroll_container"]/div[1]/div[1]/div[1]/div').get_attribute('textContent')
+                if str(now_time.month)+'-'+str(now_time.day) in text:
+                    print('Success:打卡成功!')
                 browser.quit()
             #args.once为True则立即结束程序
             if args.once : break
@@ -69,5 +76,5 @@ while True:
             print('登录失败！请检查用户名和密码是否正确')
             browser.quit()
             break
-    #每30秒查询一次时间
-    time.sleep(30)
+    #每60秒查询一次时间
+    time.sleep(60)
